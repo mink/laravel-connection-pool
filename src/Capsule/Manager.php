3af54f02c4ac6eddf615bca36698d19247112bf3
Zac Olden <zacolden@gmail.com>
2020-04-10 08:43:46 +1000

    Add capsule manager back
    Refactored version without the utility methods - has no need for them now

---
+++ src/Capsule/Manager.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace X\LaravelConnectionPool\Capsule;
+
+use Illuminate\Database\Capsule\Manager as BaseManager;
+use Illuminate\Database\Connectors\ConnectionFactory;
+use X\LaravelConnectionPool\DatabaseManager;
+
+class Manager extends BaseManager
+{
+    /**
+     * Build the database manager instance.
+     *
+     * @return void
+     */
+    protected function setupManager(): void
+    {
+        $factory = new ConnectionFactory($this->container);
+
+        $this->manager = new DatabaseManager($this->container, $factory);
+    }
+}
